d62365454c7bf7e4148ebe3fea59d8128af639f4
Jayden Irwin <me@jaydenirwin.com>
2021-03-12 18:09:46 -0500

    search

--- xLights Companion Pro/SearchView.swift
+++ xLights Companion Pro/SearchView.swift
@@ -11,26 +11,49 @@ struct SearchView: View {
     
     @State var searchString = ""
     
+    var results: [RelatedWebsite] {
+        (RelatedWebsite.websites + RelatedWebsite.facebookGroups).filter({ $0.name.lowercased().contains(searchString.lowercased()) })
+    }
+    
     var body: some View {
         NavigationView {
             VStack(spacing: 16) {
                 TextField("Search", text: $searchString)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .padding()
-                Spacer()
-                Text("Suggestions")
-                    .font(.headline)
-                Button("Snowflake Light", action: { })
-                    .foregroundColor(Color.accentColor)
-                Button("Calculate Power", action: { })
-                    .foregroundColor(Color.accentColor)
-                Button("Jingle Bells", action: { })
-                    .foregroundColor(Color.accentColor)
-                Button("Zoom", action: { })
-                    .foregroundColor(Color.accentColor)
-                Button("Videos", action: { })
-                    .foregroundColor(Color.accentColor)
-                Spacer()
+                    .padding(7)
+                    .padding(.horizontal, 25)
+                    .background(Color(.systemGray6))
+                    .cornerRadius(8)
+                    .overlay(
+                        Image(systemName: "magnifyingglass")
+                            .foregroundColor(.gray)
+                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                            .padding(.leading, 8)
+                    , alignment: .leading)
+                    .padding(.horizontal, 10)
+                if searchString == "" {
+                    Spacer()
+                    Text("Suggestions")
+                        .font(.headline)
+                    Button("Snowflake Light", action: { })
+                        .foregroundColor(Color.accentColor)
+                    Button("Calculate Power", action: { })
+                        .foregroundColor(Color.accentColor)
+                    Button("Jingle Bells", action: { })
+                        .foregroundColor(Color.accentColor)
+                    Button("Zoom", action: { })
+                        .foregroundColor(Color.accentColor)
+                    Button("Videos", action: { })
+                        .foregroundColor(Color.accentColor)
+                    Spacer()
+                } else {
+                    List {
+                        ForEach(results) { result in
+                            Button(result.name) {
+                                //
+                            }
+                        }
+                    }
+                }
             }
             .navigationTitle("Search")
         }
