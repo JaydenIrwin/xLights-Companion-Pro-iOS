b589d7ccc53ac4975e14e76d3a4a9657a314b1f0
Jayden Irwin <me@jaydenirwin.com>
2021-03-29 22:31:35 -0400

    search function

--- xLights Companion Pro/SearchView.swift
+++ xLights Companion Pro/SearchView.swift
@@ -6,19 +6,16 @@
 //
 
 import SwiftUI
+import Combine
 
 struct SearchView: View {
     
-    @State var searchString = ""
-    
-    var results: [RelatedWebsite] {
-        RelatedWebsite.websites.filter({ $0.name.lowercased().contains(searchString.lowercased()) })
-    }
+    @ObservedObject var searchModel = SearchModel()
     
     var body: some View {
         NavigationView {
             VStack(spacing: 16) {
-                TextField("Search songs", text: $searchString)
+                TextField("Search songs", text: $searchModel.searchText)
                     .padding(7)
                     .padding(.horizontal, 25)
                     .background(Color(.systemGray6))
@@ -30,27 +27,35 @@ struct SearchView: View {
                             .padding(.leading, 8)
                     , alignment: .leading)
                     .padding(.horizontal, 10)
-                if searchString == "" {
+                if searchModel.searchText == "" {
                     Spacer()
                     Text("Suggestions")
                         .font(.headline)
-                    Button("Snowflake Light", action: { })
+                    Button("Santa Baby", action: {
+                        searchModel.searchText = "Santa Baby"
+                    })
                         .foregroundColor(Color.accentColor)
-                    Button("Calculate Power", action: { })
+                    Button("We Wish You A Merry Christmas", action: {
+                        searchModel.searchText = "We Wish You A Merry Christmas"
+                    })
                         .foregroundColor(Color.accentColor)
-                    Button("Jingle Bells", action: { })
+                    Button("Jingle Bells", action: {
+                        searchModel.searchText = "Jingle Bells"
+                    })
                         .foregroundColor(Color.accentColor)
-                    Button("Zoom", action: { })
+                    Button("Deck The Halls", action: {
+                        searchModel.searchText = "Deck The Halls"
+                    })
                         .foregroundColor(Color.accentColor)
-                    Button("Videos", action: { })
+                    Button("Winter Wonderland", action: {
+                        searchModel.searchText = "Winter Wonderland"
+                    })
                         .foregroundColor(Color.accentColor)
                     Spacer()
                 } else {
                     List {
-                        ForEach(results) { result in
-                            Button(result.name) {
-                                //
-                            }
+                        ForEach(searchModel.results) { result in
+                            Link(result.title, destination: URL(string: result.url)!)
                         }
                     }
                 }
@@ -66,4 +71,79 @@ struct SearchView_Previews: PreviewProvider {
     static var previews: some View {
         SearchView()
     }
+}
+
+class SearchModel: ObservableObject {
+    
+    var subscription: Set<AnyCancellable> = []
+    
+    @Published private (set) var results: [SongResult] = []
+    
+    @Published var searchText: String = ""
+    
+    // MARK:- Initiliazer for product via model.
+    
+    init() {
+        $searchText
+            .debounce(for: .milliseconds(800), scheduler: RunLoop.main) // debounces the string publisher, such that it delays the process of sending request to remote server.
+            .removeDuplicates()
+            .map({ (string) -> String? in
+                if string.count < 1 {
+                    self.results = []
+                    return nil
+                }
+                
+                return string
+            }) // prevents sending numerous requests and sends nil if the count of the characters is less than 1.
+            .compactMap{ $0 } // removes the nil values so the search string does not get passed down to the publisher chain
+            .sink { (_) in
+                //
+            } receiveValue: { [self] (searchField) in
+                searchItems(searchText: searchField)
+            }.store(in: &subscription)
+    }
+    
+    
+    private func searchItems(searchText: String) {
+        var request = URLRequest(url: URL(string: "https://mysterious-coast-36838.herokuapp.com/feed/p")!)
+        request.httpMethod = "POST"
+        let params: [String: Any] = ["title": searchText.addingPercentEncoding(withAllowedCharacters: .alphanumerics)!]
+        request.httpBody = try! JSONSerialization.data(withJSONObject: params)
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data,
+                  let response = response as? HTTPURLResponse,
+                  error == nil else {                                              // check for fundamental networking error
+                print("error", error ?? "Unknown error")
+                return
+            }
+            
+            guard (200 ... 299) ~= response.statusCode else {                    // check for http errors
+                print("statusCode should be 2xx, but is \(response.statusCode)")
+                print("response = \(response)")
+                return
+            }
+            
+            do {
+                let songs = try JSONDecoder().decode(Results.self, from: data)
+                self.results = songs.results
+            } catch {
+                print("no results")
+            }
+        }
+        task.resume()
+    }
+}
+
+struct Results: Codable {
+    var results: [SongResult]
+}
+
+struct SongResult: Identifiable, Codable {
+    let _id: String
+    var id: String {
+        _id
+    }
+    let baseUrl: String
+    let title: String
+    let url: String
 }
