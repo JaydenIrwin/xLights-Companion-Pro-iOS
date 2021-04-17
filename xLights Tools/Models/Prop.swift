ebe53e55abd6ea2c99e0aa33b497d118f337f561
Jayden Irwin <me@jaydenirwin.com>
2021-04-16 20:35:48 -0400

    all prop images

--- xLights Tools/Models/Prop.swift
+++ xLights Tools/Models/Prop.swift
@@ -19,16 +19,16 @@ struct Prop: Identifiable {
     static let allProps = [
         Prop(iconName: "Arch", fileName: "Arch", plane: .horizontal),
         Prop(iconName: "Candy Cane", fileName: "Candy Cane", plane: .horizontal),
-        Prop(iconName: "Star", fileName: "Circle Border", plane: .any),
+        Prop(iconName: "Circle Border", fileName: "Circle Border", plane: .any),
         Prop(iconName: "Matrix", fileName: "Matrix", plane: .any),
-        Prop(iconName: "Star", fileName: "Mega Tree", plane: .horizontal),
-        Prop(iconName: "Star", fileName: "Mini Tree", plane: .horizontal),
-        Prop(iconName: "Star", fileName: "Pipe", plane: .any),
-        Prop(iconName: "Star", fileName: "Present", plane: .horizontal),
-        Prop(iconName: "Star", fileName: "Snow Flake", plane: .any),
-        Prop(iconName: "Star", fileName: "Sphere", plane: .horizontal),
+        Prop(iconName: "Mega Tree", fileName: "Mega Tree", plane: .horizontal),
+        Prop(iconName: "Mini Tree", fileName: "Mini Tree", plane: .horizontal),
+        Prop(iconName: "Pipe", fileName: "Pipe", plane: .any),
+        Prop(iconName: "Present", fileName: "Present", plane: .horizontal),
+        Prop(iconName: "Snow Flake", fileName: "Snow Flake", plane: .any),
+        Prop(iconName: "Sphere", fileName: "Sphere", plane: .horizontal),
         Prop(iconName: "Spiral Tree", fileName: "Spiral Tree", plane: .horizontal),
-        Prop(iconName: "Star", fileName: "Square Border", plane: .any),
+        Prop(iconName: "Square Border", fileName: "Square Border", plane: .any),
         Prop(iconName: "Star", fileName: "Star", plane: .any)
     ]
     
