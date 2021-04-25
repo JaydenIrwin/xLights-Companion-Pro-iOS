94778c59bfe4aa19998492d37ba8caa3520aafa7
Nicholas Long <nicholas.long123@gmail.com>
2021-04-25 14:34:05 -0400

    testing

---
+++ xLightsToolsTests/xLightsTests.swift
@@ -0,0 +1,26 @@
+//
+//  xLightsTests.swift
+//  xLightsToolsTests
+//
+//  Created by Nicholas Long on 2021-04-25.
+//
+
+import XCTest
+@testable import xLights_Tools
+
+
+class xLightsTests: XCTestCase {
+    private var powerCalculator : PowerCalculator!
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        try super.tearDownWithError()
+    }
+
+    
+
+}
